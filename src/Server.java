ca6c4957953730c8f430923157b552df6f8adfca
siraxle <sir.axle@yandex.ru>
2020-10-07 00:32:36 +0300

    add headers to server request

--- src/Server.java
+++ src/Server.java
@@ -5,13 +5,18 @@ public class Server {
   public static void main(String[] args) throws IOException {
 
     ServerSocket serverSocket = new ServerSocket(8000);
+    int count = 0;
     while (true) {
       Socket clientSocket = serverSocket.accept();
       //clientSocket.getOutputStream().write("<h2>Hello</h2>".getBytes());
-      System.out.println("client accepted");
+      System.out.println("client accepted" + (count++));
       OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream());
-      writer.write("<h1>Java</h1>");
+      writer.write("HTTP/1.0 200 OK\r\n" +
+              "Content-type: text/html\r\n"+
+              "\r\n"+
+              "<h1>Java</h1>\r\n");
       writer.flush();
+      writer.close();
       clientSocket.close();
     }
     //serverSocket.close();
